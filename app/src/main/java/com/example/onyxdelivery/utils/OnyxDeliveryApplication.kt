e554ce25fae5bfd2d1bcabc016ae163a92018cb5
Mohamed Emad <mohamedemad199912@gmail.com>
2025-05-08 03:24:40 +0300

    prepare hilt for dependency injection

---
+++ app/src/main/java/com/example/onyxdelivery/utils/OnyxDeliveryApplication.kt
@@ -0,0 +1,8 @@
+package com.example.onyxdelivery.utils
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OnyxDeliveryApplication : Application(){
+}
